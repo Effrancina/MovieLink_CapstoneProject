78d527eaa3144391f1f47f6bc444aa1b7e80def7
Romain Landolfini <romain.landolfini@gmail.com>
2023-02-07 15:18:40 +0000

    added comment

--- movieproject/src/main/java/com/example/movieproject/controllers/MovieController.java
+++ movieproject/src/main/java/com/example/movieproject/controllers/MovieController.java
@@ -24,6 +24,7 @@ public class MovieController {
         return new ResponseEntity<>(movieRepository.findAll(), HttpStatus.OK);
     }
 
+    //post method for multiple movie objects
     @PostMapping(value="/movies")
     public ResponseEntity<List<Movie>> postMovies(@RequestBody ArrayList<Movie> movies){
         for (Movie movie : movies){
