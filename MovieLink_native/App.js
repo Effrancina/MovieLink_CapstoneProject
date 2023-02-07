c0856aabac42734b78ccc82c3b26b62983f2e45b
Peter Morrison <pmorrison234@gmail.com>
2023-02-07 15:07:09 +0000

    front end connected to back end

--- MovieLink_native/App.js
+++ MovieLink_native/App.js
@@ -1,16 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-
+import  {useEffect,useState} from 'react'
 
 export default function App() {
+  const[movies,setMovies] = useState([])
+
+  useEffect( () => {
+    fetch("http://192.168.100.127:8080/movies")
+      .then (res => res.json())
+      // .then(moviesData => setMovies(moviesData))
+      .then(moviesData => setMovies(moviesData))
+      // .then(moviesData => setMovies(moviesData[0]))
+      .catch(err => console.error(err));
+  
+  }, [])
+
+  const movieItems = movies.map( (movie) => {
+    console.log(movie.title)
+    return <Text key={movie.id}>{movie.title}</Text>
+  })
+
   return (
     <View style={styles.container}>
       <MaterialCommunityIcons name="movie-roll" size={40} color="black"/>
       <Text style={styles.title}>MovieLink</Text>
       <StatusBar style="auto" />
+      <View>
+        <Text> {movieItems}</Text>
+      </View>
     </View>
+   
   );
 }
 
--- movieproject/src/test/java/com/example/movieproject/MovieprojectApplicationTests.java
+++ movieproject/src/test/java/com/example/movieproject/MovieprojectApplicationTests.java
@@ -15,14 +15,10 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 @SpringBootTest
 class MovieprojectApplicationTests {
-
 	@Autowired
 	MovieRepository movieRepository;
-
 	@Autowired
 	RegionRepository regionRepository;
-	
-
 	@Test
 	void contextLoads() {
 	}
@@ -30,9 +26,12 @@ class MovieprojectApplicationTests {
 	@Test
 	public void addMovie(){
 		Region region = new Region("UK");
-		Movie movie = new Movie("All Quiet on the Western Front");
-		region.addMovie(movie);
-		movieRepository.save(movie);
+		Movie movie1 = new Movie("All Quiet on the Western Front");
+		region.addMovie(movie1);
+		Movie movie2 = new Movie("The Grinch");
+		region.addMovie(movie2);
+		movieRepository.save(movie1);
+		movieRepository.save(movie2);
 		regionRepository.save(region);
 	}
 
