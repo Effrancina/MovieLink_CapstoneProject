b55f47136ee1720ac547d59ce2206ed0b65fef11
effrancina <f.v.menichetti@gmail.com>
2023-02-08 15:29:15 +0000

    added find all movies by region id to region controller

--- movieproject/src/main/java/com/example/movieproject/controllers/RegionController.java
+++ movieproject/src/main/java/com/example/movieproject/controllers/RegionController.java
@@ -1,12 +1,15 @@
 package com.example.movieproject.controllers;
 
+import com.example.movieproject.models.Movie;
 import com.example.movieproject.models.Region;
+import com.example.movieproject.repositories.MovieRepository;
 import com.example.movieproject.repositories.RegionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,6 +19,7 @@ public class RegionController {
 
     @Autowired
     RegionRepository regionRepository;
+    MovieRepository movieRepository;
 
     @GetMapping(value="/regions")
     public ResponseEntity<List<Region>> getAllRegions(){
@@ -27,5 +31,9 @@ public class RegionController {
         return new ResponseEntity<>(regionRepository.findById(id), HttpStatus.OK);
     }
 
+    @GetMapping(value="/regions/{id}")
+    public ResponseEntity<List<Movie>> getMoviesForRegion(@PathVariable Long id){
+        return new ResponseEntity<>(movieRepository.findAllByRegions(id), HttpStatus.OK);
+    }
 
 }
--- movieproject/src/main/java/com/example/movieproject/repositories/RegionRepository.java
+++ movieproject/src/main/java/com/example/movieproject/repositories/RegionRepository.java
@@ -1,9 +1,13 @@
 package com.example.movieproject.repositories;
 
+import com.example.movieproject.models.Movie;
 import com.example.movieproject.models.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Long> {
+
 }
